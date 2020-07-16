0b48f55958d1b21f34426f293ea95d554ef5bf0f
JanR <jasiu@360.st>
2020-07-16 17:46:43 +0200

    Added interfaces and classes for domain.core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+
+    public interface IEventHandler
+    {
+
+    }
+}
