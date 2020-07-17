dc29c7b7db7a697ec8b275d7f3b98558e33f4dc1
JanR <jasiu@360.st>
2020-07-17 11:35:41 +0200

    Fixed typo in project name, added di

---
+++ MicroRabbit/MicroRabbit.Infa.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
